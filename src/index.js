c8d1731156cb5dc86b4e6a03624292333ed7411a
TheBillyBox <47089710+TheBillyBox@users.noreply.github.com>
2021-02-09 14:11:07 +0100

    Twitch alerts

--- src/index.js
+++ src/index.js
@@ -31,5 +31,35 @@ client.on('message', message => {
         message.channel.send('Puedes invitar a gente con : https://discord.gg/s8S3WbyGWX')
         console.log('Invitación enviada')
     }
+    //Twitch
+    if (message.content === '!kelito' && message.channel.id === '748691155004424203'){
+        message.channel.send('@everyone Kelito esta en directo en twitch!! https://twitch.tv/angelsc_01')
+        console.log('Kelito directo')
+    } 
+    if (message.content === '!tobal' && message.channel.id === '748691155004424203'){
+        message.channel.send('@everyone Tobalito esta en directo en twitch!! https://twitch.tv/tobalito')
+        console.log('Tobal directo')
+    }
+    if (message.content === '!javiten' && message.channel.id === '748691155004424203'){
+        message.channel.send('@everyone Tobalito esta en directo en twitch!! https://twitch.tv/javiten51')
+        console.log('Javiten directo')
+    }
+    if (message.content === '!frutyy' && message.channel.id === '748691155004424203'){
+        message.channel.send('@everyone Tobalito esta en directo en twitch!! https://twitch.tv/fruttyy_')
+        console.log('Frutty directo')
+    }
+    if (message.content === '!albita' && message.channel.id === '748691155004424203'){
+        message.channel.send('@everyone Tobalito esta en directo en twitch!! https://twitch.tv/akalbuchi')
+        console.log('Albita directo')
+    }
+    if (message.content === '!pablo' && message.channel.id === '748691155004424203'){
+        message.channel.send('@everyone Tobalito esta en directo en twitch!! https://twitch.tv/soled_')
+        console.log('Pablo directo')
+    }
+    //Restricted
+    //if (message.content.includes('twitch.tv') && message.channel.id === "748688277346844850"){
+    //    message.delete
+    //}
+    
 });
 
